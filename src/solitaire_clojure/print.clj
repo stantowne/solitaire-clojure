589a8cd95be5b6c2af455d1973f0793fabf416fa
stantowne <41483026+stantowne@users.noreply.github.com>
2025-10-07 08:32:43 -0400

    Added (defn print-tableau . . .) to print the tableau column-wise rather than row-wise.  Also, adjusted order in which components of game-state are printed.

--- src/solitaire_clojure/print.clj
+++ src/solitaire_clojure/print.clj
@@ -9,6 +9,8 @@
     (str "\u001b[36m-" (str suit) "\u001b[0m"))) ;blue -- (for black) spades 2 and clubs 0
 )
 
+(declare print-tableau)
+
 (defn suit-str
   "suit number as string in color"
   [suit]
@@ -32,7 +34,7 @@
     (str v)))
 
 (defn card-str
-  "3 character string representation of a card, including face up or down."
+  "4 character string representation of a card, including Up or Dn."
   [card]
   (str
     (value-str (:value card))
@@ -47,18 +49,43 @@
         waste (:waste field)
         tableau (:tableau field)
         foundations (:foundations field)]
-  (println (str "Current Game State:  Game Number " game-number))
+  (println (str "Current Game State:  \nGame Number: " game-number))
+  (println (str "Moves Made: " moves-made))
   (println "Stock:" (clojure.string/join " " (map card-str stock)))
   (println "Waste:" (clojure.string/join " " (map card-str waste)))
-  (println "Tableau:")
-  (doseq [pile tableau]
-    (println (str "  " (clojure.string/join " " (map card-str pile)))))
   (println (str "Foundations: "
-            (nth foundations 0) " Clubs; "
-            (nth foundations 1) " Diamonds; "
-            (nth foundations 2) " Spades; "
-            (nth foundations 3) " Hearts"))
-  (println "Moves made:" moves-made)
+                (nth foundations 0) " Clubs; "
+                (nth foundations 1) " Diamonds; "
+                (nth foundations 2) " Spades; "
+                (nth foundations 3) " Hearts"))
+  (println "Tableau:")
+  (print-tableau tableau)
+  (println)
   (println "---------------------"))
   ;; (read-line)
   )
+
+
+
+(require '[clojure.string :as str])
+
+
+
+(defn print-tableau
+  "Prints a Solitaire tableau column-wise."
+  [tableau]
+  ;; 1. Find the height of the tallest column.
+  (let [max-height (apply max (map count tableau))]
+    ;; 2. Loop for each horizontal row to be printed.
+    (println "            0     1     2     3     4     5     6")
+    (doseq [row-index (range max-height)]
+      ;; 3. For the current row, build a sequence of card strings.
+      (let [row-cards (for [col-index (range 7)]
+                        (let [;; Get the card at [column, row], or nil if it doesn't exist.
+                              card (get-in tableau [col-index row-index])]
+                          (if card
+                            (card-str card)
+                            "    "))) ; 4. Use a placeholder for empty spots.
+            row-str (str/join "  " row-cards)]
+
+        (println "          " row-str)))))
