15507ee39f3c7f6bf720eca9985bd900866a4df0
stantowne <41483026+stantowne@users.noreply.github.com>
2025-10-20 15:50:25 -0400

    Added additional printing to flip-three-forward and flip

--- src/solitaire_clojure/flip.clj
+++ src/solitaire_clojure/flip.clj
@@ -14,7 +14,14 @@
         new-waste (vec (concat waste (map force-card-face-up (reverse (subvec stock (- stock-count 3) stock-count)))))
         new-field (assoc field :stock new-stock :waste new-waste)
         new-moves-made (inc moves-made)]
-     (assoc game-state :field new-field :moves-made new-moves-made)))
+    ;; (println "leaving flip-three-forward")
+    ;; (println "stock count: " (count (:stock (:field game-state))))
+    ;; (println "waste count: " (count (:waste (:field game-state))))
+    ;; (println "new stock count: " (count new-stock))
+    ;; (println "new waste count: " (count new-waste))
+    (do
+      (println "flip-three-forward")
+      (assoc game-state :field new-field :moves-made new-moves-made))))
 
 
 
@@ -42,9 +49,14 @@
             flip-times (max 0 (min (- initial-flips-of-round 1) (- (quot (count new-stock) 3) 1))) ;; how many times to flip three forward
             new-field (assoc field :stock new-stock :waste new-waste :initial-flips-of-round flip-times)
             new-moves-made (inc moves-made)
-            flip-times (max 0 (min (- initial-flip-count-at-start-of-round 1) (- (quot (count new-stock) 3) 1)))] ;; how many times to flip three forward
-        (assoc game-state :field new-field :moves-made new-moves-made :initial-flip-count-at-start-of-round flip-times)
-        (nth (iterate flip-three-forward game-state) flip-times)) ;; now flip three forward
+            new-game-state (assoc game-state :field new-field :moves-made new-moves-made )]
+        ;; (println "stock-count: " stock-count)
+        ;; (println "stock: " stock)
+        ;; (println "waste: " waste)
+        ;; (println "new-stock: " new-stock)
+        ;; (println "new-waste: " new-waste)
+        ;; (println "flip-times: " flip-times)
+        (nth (iterate flip-three-forward new-game-state) flip-times)) ;; now flip three forward
 
       (> stock-count 2) ;; three or more cards in stock
       (flip-three-forward game-state)
