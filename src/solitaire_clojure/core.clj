f88490a73b51d5caa6cb4ba6cd7f1f606c7528f8
stantowne <41483026+stantowne@users.noreply.github.com>
2025-10-23 17:55:25 -0400

    split the move making function out of the game playing function.  Next step will be to use an atom for the game state. Win 1395

--- src/solitaire_clojure/core.clj
+++ src/solitaire_clojure/core.clj
@@ -1,21 +1,16 @@
 (ns solitaire-clojure.core
   (:gen-class)
-  (:require [solitaire-clojure.print
-             :refer [print-game-state]])
-  (:require [solitaire-clojure.helper-functions
-             :refer [force-card-face-up]])
-  (:require [solitaire-clojure.moves
-             :refer [move-a-card-from-pile-to-foundations
-                     move-a-card-from-waste-to-foundations
-                     move-a-card-from-waste-to-pile
-                     flip]])
-  (:require [solitaire-clojure.move-partial-pile
-             :refer [move-partial-pile]])
-  (:require [solitaire-clojure.move-entire-pile
-             :refer [move-entire-pile]])
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [solitaire-clojure.helper-functions :refer [force-card-face-up]]))
+            [solitaire-clojure.print :refer [print-game-state]]
+            [solitaire-clojure.helper-functions :refer [force-card-face-up]]
+            [solitaire-clojure.moves :refer [move-a-card-from-pile-to-foundations
+                                             move-a-card-from-waste-to-foundations
+                                             move-a-card-from-waste-to-pile
+                                             flip]]
+            [solitaire-clojure.move-partial-pile :refer [move-partial-pile]]
+            [solitaire-clojure.move-entire-pile :refer [move-entire-pile]]))
+
 
 (defonce csv-reader-atom (atom nil))
 
@@ -60,35 +55,11 @@
                                        [(nth sd 45) (nth sd 39) (nth sd 34) (nth sd 30) (nth sd 27) (nth sd 25) (force-card-face-up (nth sd 24))]]
                          :foundations [0 0 0 0]}
                         :moves-made 0
-                        :seen-fields []}]
+                        :seen-fields []
+                        :game-result :in-progress}]
         game-state))))
 
-(comment
-  ;; Creates an unshuffled deck of cards
-  (def unshuffled-deck
-    (vec (for [s (range 4) ; hearts 0, spades 1, diamonds 2, clubs 3
-               v (range 1 14)]
-           {:suit s :value v :face-up false})))
 
-  (defn shuffle-and-deal
-    "Returns a map with three first level keys -- :field, :moves-made, and :seen-fields. Shuffles the deck and then deals it out to initial condition"
-    [deck]
-    (let [sd (vec (shuffle deck))
-          game-state {:field
-                      {:stock       (subvec sd 0 24)
-                       :waste       []
-                       :tableau     [[(force-card-face-up (nth sd 51))]
-                                     [(nth sd 50) (force-card-face-up (nth sd 44))]
-                                     [(nth sd 49) (nth sd 43) (force-card-face-up (nth sd 38))]
-                                     [(nth sd 48) (nth sd 42) (nth sd 37) (force-card-face-up (nth sd 33))]
-                                     [(nth sd 47) (nth sd 41) (nth sd 36) (nth sd 32) (force-card-face-up (nth sd 29))]
-                                     [(nth sd 46) (nth sd 40) (nth sd 35) (nth sd 31) (nth sd 28) (force-card-face-up (nth sd 26))]
-                                     [(nth sd 45) (nth sd 39) (nth sd 34) (nth sd 30) (nth sd 27) (nth sd 25) (force-card-face-up (nth sd 24))]]
-                       :foundations [0 0 0 0]}
-                      :moves-made  0
-                      :seen-fields []}]
-      game-state))
-  )
 ;; The result of this function is a map with three keys: :field, :moves-made, and :seen-fields
 ;; :field is itself a map with four keys: :stock, :waste, :tableau, and :foundations
  ;; :stock is a vector of cards (maps with keys :suit, :value, and :face-up)
@@ -99,59 +70,48 @@
 ;; :seen-fields is a set of previous fields to detect loops
 ;;
 
+(def game-state-atom (atom (deal-next-deck)))
 
+(defn game-over? [current-state]
+  (not= (:game-result current-state) :in-progress))
 
-
-(defn play-game
-  ([game-state]
-   (loop [game-state game-state]
-     (if (> (:moves-made game-state) (:print-if-move-count-exceeds config))  ;; because the initial state is printed in core.clj -main
-       (print-game-state game-state))
+(defn find-and-make-move
+  ([current-state]
+   (let [{:keys [field moves-made seen-fields]} current-state]
      (cond
-      (= (reduce + (:foundations (:field game-state))) 52)
-        {:result :won}
-      (= (:moves-made game-state) (:move-limit config))
-        {:result :lost-limit-reached}
-      (some #(= % (:field game-state)) (:seen-fields game-state))
-        (let [idx-pair (some #(when (= (second %) (:field game-state)) %) (map-indexed vector (:seen-fields game-state)))]
-          (when idx-pair
-            {:result :lost-field-repeated :index (first idx-pair)}))
-      :else
-         (let [new-seen-fields (conj (:seen-fields game-state) (:field game-state))
-               game-state (assoc game-state :seen-fields new-seen-fields)]
-            (if-let [result (move-a-card-from-pile-to-foundations game-state 2 13)]
-              (recur result)
-              (do
-                (when (:print-func-failure config) (println "move-a-card-from-pile-to-foundations (2) failed"))
-                (if-let [result (move-a-card-from-waste-to-foundations game-state 2)]
-                  (recur result)
-                  (do
-                    (when (:print-func-failure config) (println "move-a-card-from-waste-to-foundations (2) failed"))
-                    (if-let [result (move-a-card-from-waste-to-pile game-state)]
-                      (recur result)
-                      (do
-                        (when (:print-func-failure config) (println "move-a-card-from-waste-to-pile failed"))
-                        (if-let [result (move-entire-pile game-state)]
-                          (recur result)
-                          (do
-                            (when (:print-func-failure config) (println "move-entire-pile failed"))
-                            (if-let [result (move-partial-pile game-state)]
-                              (recur result)
-                              (do
-                                (when (:print-func-failure config) (println "move-partial-pile failed"))
-                                (if-let [result (move-a-card-from-pile-to-foundations game-state 13 9)]
-                                  (recur result)
-                                  (do
-                                    (when (:print-func-failure config) (println "move-a-card-from-pile-to-foundations (13) failed"))
-                                    (if-let [result (move-a-card-from-waste-to-foundations game-state 13)]
-                                      (recur result)
-                                      (do
-                                        (when (:print-func-failure config) (println "move-a-card-from-waste-to-foundations (13) failed"))
-                                        (if-let [result (flip game-state)]
-                                          (recur result)
-                                          (do
-                                            (when (:print-func-failure config) (println "flip failed"))
-                                            game-state)))))))))))))))))))))
+       (= (reduce + (:foundations field)) 52)
+       (assoc current-state :game-result :won)
+
+       (= moves-made (:move-limit config)) ;; impure
+       (assoc current-state :game-result :lost-limit-reached)
+
+       (some #(= % field) seen-fields)
+       (assoc current-state :game-result :lost-field-repeated)
+
+       :else
+       (let [new-seen-fields (conj seen-fields field)
+             current-state (assoc current-state :seen-fields new-seen-fields)
+             movers [#(move-a-card-from-pile-to-foundations % 2 13)
+                     #(move-a-card-from-waste-to-foundations % 2)
+                     move-a-card-from-waste-to-pile
+                     move-entire-pile
+                     move-partial-pile
+                     #(move-a-card-from-pile-to-foundations % 13 9)
+                     #(move-a-card-from-waste-to-foundations % 13)
+                     flip]]
+         (or (some #(% current-state) movers)
+             current-state))))))
+
+(defn calculate-next-state [current-state]
+  (if (game-over? current-state)
+    current-state
+    (find-and-make-move current-state)))
+
+(defn play-game [initial-state]
+  (loop [current-state initial-state]
+    (if (game-over? current-state)
+      {:result (:game-result current-state)}
+      (recur (calculate-next-state current-state)))))
 
 (defn -main
   "Main entry point for the Solitaire game"
@@ -160,8 +120,8 @@
   (let [[_ final-results]
          (loop [deck-number (:first-deck-num config)
                 record-of-results {:lost-limit-reached 0 :lost-field-repeated 0 :won 0}]
-          (if (< deck-number (+ (:first-deck-num config) (:num-of-decks config))) ;; change to 10000 for full run
-            (let [game-state (assoc (deal-next-deck) :deck-number deck-number)
+          (if (< deck-number (+ (:first-deck-num config) (:num-of-decks config)))
+            (let [game-state (deal-next-deck)
                   result (play-game game-state)
                   updated-results
                     (cond
