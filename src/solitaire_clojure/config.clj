e5437df863432822b18866e8bfb50af53223429f
stantowne <41483026+stantowne@users.noreply.github.com>
2025-10-31 23:03:05 -0400

    Moved config to edn file.

--- src/solitaire_clojure/config.clj
+++ src/solitaire_clojure/config.clj
@@ -1,12 +1,12 @@
-(ns solitaire-clojure.config)
+(ns solitaire-clojure.config
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
 
+(defn- load-config [filename]
+  (-> (io/resource filename) ; 1. Find the file in 'resources'
+      (slurp)                ; 2. Read it into a single string
+      (edn/read-string)))    ; 3. Parse the string into a Clojure map
 
-
-(def config
-  {:decks-filepath     "test/resources/decks-made-2022-01-15-count-10000-dict.csv"
-   :first-deck-num     1 ; files are treated as 0 base
-   :num-of-decks       1 ;; do not exceed number of decks in file minus first-deck-num
-   :move-limit         200
-   :print-each-move?   true
-   :interactive-mode?  true})
+;; This 'def' now creates the 'config' var by loading it from the file.
+(def config (load-config "config.edn"))
 
